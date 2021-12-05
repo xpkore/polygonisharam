ea8178f0eea51e380892731fa3c47e77ecc33ce3
philzeh <philzeh@gmail.com>
2021-12-05 23:27:07 +1100

    block

--- Haram1.cs
+++ Haram1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static DEFI.haram.Haram2;
 
 namespace haram
 {
@@ -12,7 +13,14 @@ namespace haram
         {
             Haram2 client = new Haram2(url);
             client.Start();
-            client.SubscribePendingTransactions(PendingTransactionHandler);
+            client.SubscribeBlockHeaders(BlockHeadersHandler);
+            //client.SubscribePendingTransactions(PendingTransactionHandler);
+        }
+
+        private void BlockHeadersHandler(BlockHeaderResult obj)
+        {
+            long s = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+            Console.WriteLine(s + " : " + obj.number);
         }
 
         private void PendingTransactionHandler(string obj)
--- Haram2.cs
+++ Haram2.cs
@@ -41,6 +41,38 @@ namespace DEFI.haram
             }
         }
 
+        public async void SubscribeBlockHeaders(Action<BlockHeaderResult> action)
+        {
+            try
+            {
+                var postObj = new
+                {
+                    @jsonrpc = "2.0",
+                    @id = 1,
+                    @method =
+                "eth_subscribe",
+                    @params = new object[] {
+                    "newHeads"
+                }
+                };
+                string body = JsonConvert.SerializeObject(postObj);
+                SendRequest(body);
+                string subscriptionId = GetNextSubscriptionId();
+
+                SubscriptionDictionary[subscriptionId] = new Action<object>((model) =>
+                {
+                    string modelString = model.ToString();
+                    BlockHeaderResult modelObj = JsonConvert.DeserializeObject<BlockHeaderResult>(modelString);
+                    action(modelObj);
+                });
+                Console.WriteLine("Subscribed");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception");
+            }
+        }
+
         public void SubscribePendingTransactions(Action<string> action)
         {
             try
@@ -165,6 +197,27 @@ namespace DEFI.haram
         {
             Console.WriteLine("Close called");
             Socket.Close();
+        }
+
+        public class BlockHeaderResult
+        {
+            public string parentHash { get; set; }
+            public string sha3Uncles { get; set; }
+            public string miner { get; set; }
+            public string stateRoot { get; set; }
+            public string transactionsRoot { get; set; }
+            public string receiptsRoot { get; set; }
+            public string logsBloom { get; set; }
+            public string difficulty { get; set; }
+            public string number { get; set; }
+            public string gasLimit { get; set; }
+            public string gasUsed { get; set; }
+            public string timestamp { get; set; }
+            public string extraData { get; set; }
+            public string mixHash { get; set; }
+            public string nonce { get; set; }
+            public string hash { get; set; }
+            public string baseFeePerGas { get; set; }
         }
 
     }
